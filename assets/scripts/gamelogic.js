b21ba4916c3c4b7f54763b2dbf0d49faf6144422
shawnfraistat <shawn.fraistat@gmail.com>
2018-10-31 15:43:25 -0400

    Fix Multiple Clicks on Same Board space
    Players were able to click multiple times on the same board space,
    because I hadn't updated the innerHTML readyPlayerTurn() was checking
    for. Now it correctly rules out spots that have been taken.

--- assets/scripts/gamelogic.js
+++ assets/scripts/gamelogic.js
@@ -2,8 +2,6 @@ const ai = require('./ai.js')
 const ui = require('./ui.js')
 const store = require('./store.js')
 
-const aiMark = 'o'
-
 // takeAITurn() calls ai.getAIMove to calculate a move for the ai player. It then updates the board display,
 // evaluates the board to see if it won, and readies the game for the human player's move if the game isn't over yet
 
@@ -13,7 +11,7 @@ const takeAITurn = function () {
   console.log('Inside of takeAITurn, aiDifficulty is', store.aiDifficulty)
   const aiMove = ai.getAIMove(store.currentBoard, store.aiDifficulty)
   console.log('Inside of takeAITurn, aiMove is', aiMove)
-  store.currentBoard[aiMove] = aiMark
+  store.currentBoard[aiMove] = 'o'
   ui.updateBoardDisplay(store.currentBoard)
   if (ai.terminalCheck(store.currentBoard, 'playerTwo') === 'playerTwoWin') {
     ui.showPlayerWin('playerTwo')
@@ -31,7 +29,7 @@ const takeAITurn = function () {
 const readyPlayerTurn = function () {
   ui.showPlayerTurn()
   for (let i = 0; i < 9; i++) {
-    if (document.getElementById(i).innerHTML !== '<p class="red">X</p>' && document.getElementById(i).innerHTML !== '<p class="blue">O</p>') {
+    if (document.getElementById(i).innerHTML !== '<p class="x-color">X</p>' && document.getElementById(i).innerHTML !== '<p class="o-color">O</p>') {
       document.getElementById(i).addEventListener('click', takeHumanTurn)
     }
   }
@@ -41,15 +39,15 @@ const readyPlayerTurn = function () {
 // It updates the board and checks to see if the human won or tied. Then it lets the ai player go.
 
 const takeHumanTurn = function (event) {
+  for (let i = 0; i < 9; i++) {
+    document.getElementById(i).removeEventListener('click', takeHumanTurn)
+  }
   if (store.currentPlayer === 'playerOne') {
     store.currentBoard[event.srcElement.id] = 'x'
   } else if (store.currentPlayer === 'playerTwo') {
     store.currentBoard[event.srcElement.id] = 'o'
   }
   ui.updateBoardDisplay(store.currentBoard)
-  for (let i = 0; i < 9; i++) {
-    document.getElementById(i).removeEventListener('click', takeHumanTurn)
-  }
   if (ai.terminalCheck(store.currentBoard, 'playerOne') === 'playerOneWin') {
     console.log('Player One wins!')
     ui.showPlayerWin('playerOne')

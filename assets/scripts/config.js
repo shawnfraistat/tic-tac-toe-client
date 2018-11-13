07ff82cc6a21f8500ea03f152f9caf8deba25d98
shawnfraistat <shawn.fraistat@gmail.com>
2018-11-13 12:48:23 -0500

    Updated Config.js with New API URL

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -6,7 +6,8 @@ let apiUrl
 const apiUrls = {
   oldDevelopment: 'https://tic-tac-toe-wdi.herokuapp.com',
   development: 'https://stormy-atoll-66060.herokuapp.com',
-  production: 'https://aqueous-atoll-85096.herokuapp.com'
+  oldProduction: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://stormy-atoll-66060.herokuapp.com'
 }
 
 if (window.location.hostname === 'localhost') {
